6c4d753c269824300bcc9aa23b3d2e52e0f35660
Viktoria Verkholat <peremoga2009@gmail.com>
2025-05-10 22:27:29 +0300

    flask commit hw

--- models/pydantic/students/student_read.py
+++ models/pydantic/students/student_read.py
@@ -1,11 +1,15 @@
 from datetime import date
-
+from typing import Optional
 from pydantic import BaseModel, ConfigDict, Field
 
 
 class StudentReadModel(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
-    student_id: int = Field(alias="id")
     name: str
+    surname: str
+    email: str
+    group: str
+    course_name: Optional[str] = None
+    photo_url: Optional[str] = None
     birth_date: date
--- models/sqlalchemy/student.py
+++ models/sqlalchemy/student.py
@@ -1,10 +1,23 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Date
 from database import db
+import datetime
 
 
 class Student(db.Model):
     __tablename__ = 'students'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    birth_date = db.Column(db.Date, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    birth_date: Mapped[Date] = mapped_column(Date, nullable=False)
+    course_name: Mapped[str] = mapped_column(String, nullable=True)
+    photo_url: Mapped[str] = mapped_column(String, nullable=True)
+
+    def age(self):
+        today = datetime.date.today()
+        return today.year - self.birth_date.year - (
+                    (today.month, today.day) < (self.birth_date.month, self.birth_date.day))
+
+    def __repr__(self):
+        return f"<Student(id={self.id}, name='{self.name}', surname='{self.surname}')>"
