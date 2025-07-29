8e949cd5279a883425b597beb581c7eee329e7cd
Mark_Dalakov <markdalakov@gmail.com>
2025-07-29 15:42:45 +0300

    second_task

--- app.py
+++ app.py
@@ -26,6 +26,9 @@ def init_app(app):
     db.init_app(app)
     migrate.init_app(app, db)
 
+    with app.app_context():
+        db.create_all()
+
 
 if __name__ == "__main__":
     app = create_app()
--- models/pydantic/students/student_read.py
+++ models/pydantic/students/student_read.py
@@ -1,11 +1,20 @@
+from pydantic import BaseModel, computed_field
 from datetime import date
-
-from pydantic import BaseModel, ConfigDict, Field
-
+from typing import Optional
 
 class StudentReadModel(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-
-    student_id: int = Field(alias="id")
+    id: int
     name: str
     birth_date: date
+    course: Optional[str]
+    photo_url: Optional[str]
+
+    @computed_field(return_type=int)
+    def age(self) -> int:
+        today = date.today()
+        return today.year - self.birth_date.year - (
+            (today.month, today.day) < (self.birth_date.month, self.birth_date.day)
+        )
+
+    class Config:
+        from_attributes = True
